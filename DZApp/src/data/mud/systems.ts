c7b331e5b26b68938ee289cee3a77071a02fe310
Ocky <>
2025-04-30 09:19:11 +0200

    Added MUD system and table definitions for minehaul.

---
+++ DZApp/src/data/mud/systems.ts
@@ -0,0 +1,31 @@
+import { resourceToHex } from "@latticexyz/common";
+
+export const networkSystem = resourceToHex({
+  type: "system",
+  namespace: "AWAR",
+  name: "NetworkSystem",
+});
+
+export const depotSystem = resourceToHex({
+  type: "system",
+  namespace: "AWAR",
+  name: "DepotSystem",
+});
+
+export const operationSystem = resourceToHex({
+  type: "system",
+  namespace: "AWAR",
+  name: "OperationSystem",
+});
+
+export const actionSystem = resourceToHex({
+  type: "system",
+  namespace: "AWAR",
+  name: "ActionSystem",
+});
+
+export const transactSystem = resourceToHex({
+  type: "system",
+  namespace: "AWAR",
+  name: "TransactSystem",
+});
