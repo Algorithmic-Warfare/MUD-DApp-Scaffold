46a270d30434bbd32cb6eb0691e43c5b7302747b
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-04-30 15:54:04 +0200

    Transaction POD issuer

--- DZApp/src/data/zupass/usePodVault.ts
+++ DZApp/src/data/zupass/usePodVault.ts
@@ -2,13 +2,27 @@ import { useCallback, useState } from "react";
 import { connect as connectZapp, ParcnetAPI } from "@parcnet-js/app-connector";
 import * as p from "@parcnet-js/podspec";
 import { zAppConfig } from "./config";
-import { POD } from "@pcd/pod";
+import { POD, PODEntries } from "@pcd/pod";
 
 export type PodVaultStatus = "offline" | "connecting" | "connected";
 
 const query = p.pod({
   entries: {},
 });
+
+export enum TransactionType {
+  Withdraw = 0,
+  Deposit = 1,
+}
+
+export type TransactionPodEntries = {
+  transactionType: TransactionType;
+  itemId: bigint;
+  itemQuantity: bigint;
+  transacteeAddress: string;
+  SsuId: bigint;
+  timestamp: bigint;
+};
 
 export function usePodVault() {
   const [status, setStatus] = useState<PodVaultStatus>("offline");
@@ -74,5 +88,42 @@ export function usePodVault() {
     },
     [zApp]
   );
-  return { status, pods, connect, store, prove, remove };
+
+  const issueTransactionPod = useCallback(
+    async (transaction: TransactionPodEntries) => {
+      if (!zApp) {
+        throw new Error("Zapp not connected");
+      }
+      const podData: PODEntries = {
+        pod_type: { type: "string", value: "hacking-protocol.transaction" },
+        transactionType: {
+          type: "int",
+          value: BigInt(transaction.transactionType),
+        },
+        itemId: { type: "int", value: transaction.itemId },
+        itemQuantity: {
+          type: "int",
+          value: transaction.itemQuantity,
+        },
+        transacteeAddress: {
+          type: "string",
+          value: transaction.transacteeAddress,
+        },
+        SsuId: { type: "int", value: transaction.SsuId },
+        timestamp: {
+          type: "date",
+          value: new Date(Number(transaction.timestamp)),
+        },
+      };
+      const signedPod = await zApp.pod.sign(podData);
+      return POD.load(
+        signedPod.entries,
+        signedPod.signature,
+        signedPod.signerPublicKey
+      );
+    },
+    [zApp]
+  );
+
+  return { status, pods, connect, store, prove, remove, issueTransactionPod };
 }
--- DZApp/src/views/dev/DevIndex.tsx
+++ DZApp/src/views/dev/DevIndex.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-import { PodVaultDemo } from "./PodVaultDemo";
-import { PodIssuerDemo } from "./PodIssuerDemo";
+import { ZupassContextProvider } from "src/data/zupass/ZupassContextProvider";
+import DevPages from "./DevPages";
 
 function DevIndex() {
-  const [page, setPage] = useState<"vault" | "issuer">("vault");
-
   return (
-    <div className="p-2">
-      <div className="flex justify-center items-center gap-4">
-        <button onClick={() => setPage("vault")} className="white-button">
-          Vault
-        </button>
-        <button onClick={() => setPage("issuer")} className="white-button">
-          Issuer
-        </button>
-      </div>
-      {page === "vault" && <PodVaultDemo />}
-      {page === "issuer" && <PodIssuerDemo />}
-    </div>
+    <ZupassContextProvider>
+      <DevPages />
+    </ZupassContextProvider>
   );
 }
 
