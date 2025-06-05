4fb78f0ad45c0b2e5511c72588f8ec613da07166
Abderraouf Belalia <abderraoufbelalia@symplectic.link>
2025-06-05 16:02:21 +0200

    fix(contracts): All tests are passing.

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,147 +1,28 @@
 import { defineWorld } from "@latticexyz/world";
 
 export default defineWorld({
-  namespace: "AWAR",
+  namespace: "TASK",
   systems: {
-    LogisticProviderSystem: {
-      name: "ProviderSystem",
-      openAccess: true,
-    },
-    LogisticCoordinatorSystem: {
-      name: "CoordinateSystem",
-      openAccess: true,
-    },
-    LogisticAgentSystem: {
-      name: "AgentSystem",
-      openAccess: true,
-    },
-    LogisticNetworkSystem: {
-      name: "NetworkSystem",
-      openAccess: true,
-    },
-    LogisticDepotSystem: {
-      name: "DepotSystem",
-      openAccess: true,
-    },
-    LogisticFixtureSystem: {
-      name: "FixtureSystem",
-      openAccess: true,
-    },
-    LogisticOperationSystem: {
-      name: "OperationSystem",
-      openAccess: true,
-    },
-    LogisticActionSystem: {
-      name: "ActionSystem",
-      openAccess: true,
-    },
-    LogisticTransactionSystem: {
-      name: "TransactSystem",
+    TaskSystem: {
+      name: "TaskSystem",
       openAccess: true,
     },
   },
   tables: {
-    // Logistic Actors
-    LogisticProvider: {
+    Tasklist: {
       schema: {
-        id: "uint256",
+        id: "bytes32",
+        creator: "address",
+        assignee: "address",
+        deadline: "uint256",
         timestamp: "uint256",
-        smartCharacterAddress: "address",
-      },
-      key: ["id"],
-    },
-    LogisticCoordinator: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        smartCharacterAddress: "address",
-        networkId: "uint256",
-      },
-      key: ["id"],
-    },
-    LogisticAgent: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        smartCharacterAddress: "address",
-        operationId: "uint256",
-      },
-      key: ["id"],
-    },
-    // Logistic Infrastructure
-    LogisticNetwork: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        providerId: "uint256",
-        codename: "string",
-        depotIds: "uint256[]",
-        coordinatorIds: "uint256[]",
-        fixtureIds: "uint256[]",
-      },
-      key: ["id"],
-    },
-    LogisticDepot: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        providerId: "uint256",
-        smartStorageUnitId: "uint256",
-        codename: "string",
-      },
-      key: ["id"],
-    },
-    LogisticFixture: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        providerId: "uint256",
-        fixtureType: "LogisticFixtureType",
-        codename: "string",
-      },
-      key: ["id"],
-    },
-    // Logistic Process
-    LogisticOperation: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        coordinatorId: "uint256",
-        codename: "string",
-        agentIds: "uint256[]",
-      },
-      key: ["id"],
-    },
-    LogisticAction: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        sourceId: "uint256",
-        destinationId: "uint256",
-        actionItemId: "uint256",
-        actionItemAmount: "uint256",
-        operationId: "uint256",
-        actionType: "LogisticActionType",
-      },
-      key: ["id"],
-    },
-    LogisticTransaction: {
-      schema: {
-        id: "uint256",
-        timestamp: "uint256",
-        transactionItemId: "uint256",
-        transactionItemAmount: "uint256",
-        agentId: "uint256",
-        depotId: "uint256",
-        actionId: "uint256",
-        transactionType: "LogisticTransactionType",
+        status: "TaskStatus",
+        description: "string",
       },
       key: ["id"],
     },
   },
   enums: {
-    LogisticFixtureType: ["FAUCET", "SINK"],
-    LogisticActionType: ["INJECT", "TRANSFER", "EXTRACT"],
-    LogisticTransactionType: ["WITHDRAWAL", "DEPOSIT"],
+    TaskStatus: ["OPEN", "CLOSED"],
   },
 });
---
+++ packages/contracts/src/systems/Tasklist/TaskSystem.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { System } from "@latticexyz/world/src/System.sol";
+import { Tasklist, TasklistData } from "@store/index.sol";
+import { TaskStatus } from "@store/common.sol";
+import "./Errors.sol";
+
+contract TaskSystem is System {
+  event TaskCreated(
+    bytes32 indexed taskId,
+    address indexed creator,
+    address indexed assignee,
+    string description,
+    uint256 deadline,
+    uint256 timestamp
+  );
+
+  event TaskUpdated(bytes32 indexed taskId, address newAssignee, string newDescription, uint256 newDeadline);
+  event TaskCompleted(bytes32 indexed taskId);
+
+  modifier onlyCreator(bytes32 taskId) {
+    TasklistData memory task = Tasklist.get(taskId);
+    if (task.creator != _msgSender()) revert Unauthorized();
+    _;
+  }
+
+  modifier onlyExistentTask(bytes32 taskId) {
+    TasklistData memory task = Tasklist.get(taskId);
+    if (task.creator == address(0)) revert TaskNotFound();
+    if (task.assignee == address(0)) revert InvalidAssignee();
+    if (task.deadline <= task.timestamp) revert InvalidDeadline();
+
+    _;
+  }
+
+  function createTask(address assignee, string memory description, uint256 deadline) public returns (bytes32 taskId) {
+    if (assignee == address(0)) revert InvalidAssignee();
+    if (deadline <= block.timestamp) revert InvalidDeadline();
+
+    taskId = keccak256(abi.encode(description, deadline, _msgSender(), block.timestamp));
+    Tasklist.set(
+      taskId,
+      TasklistData({
+        creator: _msgSender(),
+        assignee: assignee,
+        description: description,
+        timestamp: block.timestamp,
+        deadline: deadline,
+        status: TaskStatus.OPEN
+      })
+    );
+    emit TaskCreated(taskId, _msgSender(), assignee, description, deadline, block.timestamp);
+  }
+
+  function updateTaskAssignee(bytes32 taskId, address newAssignee) public onlyExistentTask(taskId) onlyCreator(taskId) {
+    TasklistData memory task = Tasklist.get(taskId);
+    if (newAssignee == address(0)) revert InvalidAssignee();
+
+    Tasklist.setAssignee(taskId, newAssignee);
+
+    emit TaskUpdated(taskId, newAssignee, task.description, task.deadline);
+  }
+
+  function updateTaskDeadline(bytes32 taskId, uint256 newDeadline) public onlyExistentTask(taskId) onlyCreator(taskId) {
+    TasklistData memory task = Tasklist.get(taskId);
+    if (newDeadline <= task.timestamp) revert InvalidDeadline();
+
+    Tasklist.setDeadline(taskId, newDeadline);
+
+    emit TaskUpdated(taskId, task.assignee, task.description, newDeadline);
+  }
+
+  function updateTaskDescription(
+    bytes32 taskId,
+    string memory newDescription
+  ) public onlyExistentTask(taskId) onlyCreator(taskId) {
+    TasklistData memory task = Tasklist.get(taskId);
+
+    Tasklist.setDescription(taskId, newDescription);
+    emit TaskUpdated(taskId, task.assignee, newDescription, task.deadline);
+  }
+
+  function completeTask(bytes32 taskId) public onlyExistentTask(taskId) onlyCreator(taskId) {
+    Tasklist.setStatus(taskId, TaskStatus.CLOSED);
+
+    emit TaskCompleted(taskId);
+  }
+}
